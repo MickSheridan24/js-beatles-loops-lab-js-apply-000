6fa35c306a7c7b36c94bd826a998a6361e8aac21
Michael Sheridan <micksheridan.24@gmail.com>
2019-01-04 19:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+var theBeatlesPlay = function (musicians, instruments){
+  let out = [];
+  
+  for (let x = 0; x < musicians.length; x++ ){
+     out.push(musicians[x] + " plays " + instruments[x]);
+  }
+}
