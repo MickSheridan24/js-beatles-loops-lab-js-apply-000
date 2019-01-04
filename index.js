c8a4f5bbbc0b92623a91d0d7ef110a5ef3c93fb7
Michael Sheridan <micksheridan.24@gmail.com>
2019-01-04 19:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ var theBeatlesPlay = function (musicians, instruments){
   for (let x = 0; x < musicians.length; x++ ){
      out.push(musicians[x] + " plays " + instruments[x]);
   }
+  return out;
 }
